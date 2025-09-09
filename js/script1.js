37776a5437a62932280d5247cabb5a64cc314a8d
Muhammad Mujahidin Eko Prasetyo <158280679+mujahidcorporation@users.noreply.github.com>
2025-09-09 13:02:29 +0700

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,38 +1,66 @@
 const ebooks = [
   {
-    title: "سفينة النجا",
+    title: "Safinatu najah",
     author: "Syekh Salim Bin Stumair al Hadromi",
     category: "fiqih",
     description: "kitab yang sangat mudah di pelajari untuk orang awam yang mau mempelajari hukum hukum islam",
     pdf: "pdf/safinatunajah.pdf"
   },
   {
-    title: "نصائح العباد",
+    title: "nashoihul ibad",
     author: "Syekh Nawawi Al Bantani",
     category: "tasawuf",
     description: "Kitab ini berisi nasehat nasehat",
     pdf: "pdf/Nashoihul Ibad.pdf"
   },
   {
-    title: "نيل الرجاء",
+    title: "Nailul Roja'",
     author: "Habib Ahmad Bin Umar Asy-Syathiri",
     category: "fiqih",
     description: "Kitab ini adalah penjelas dari kirab safinatunajah",
     pdf: "pdf/نيل الرجاء.pdf"
   },
    {
-    title: "مختارالحديث الشريف",
+    title: "Mukhtarul Hadist Syarif",
     author: "Habib umar bin salim bin hafidz",
     category: "hadist",
     description: "Kitab ini adalah berisi hadist hadist pilihan",
     pdf: "pdf/Kitab - Mukhtarul Hadits  min Syifa'is Saqim.pdf"
   },
   {
-    title: "راتب العطاس",
+    title: "Ratib Al-Athas",
     author: "Habib Umar Bin Abdurahman Al-Athos",
     category: "wirid",
     description: "Kitab ini susunan doa",
     pdf: "pdf/Mengirim email راتب العطاس.pdf"
+  },
+  {
+  title: "Doa Penutup Majelis Al-Bantani",
+    author: "Kyai Fakhrudin Al-Bantani",
+    category: "wirid",
+    description: "Kitab ini susunan doa",
+    pdf: "pdf/Doa Penutup Majelis Al Bantai 2020.pdf"
+  },
+  {
+  title: "Mulakhosa Ahkamu al Udhiya",
+    author: "Alwi Bin Abdul Qodir Assaqof",
+    category: "fiqih",
+    description: "Kumpulan Fiqih Seputat qurban",
+    pdf: "pdf/ar_molakhs_a7kam_elodhea.pdf"
+  },
+  {
+  title: "tahlilu kamalat",
+    author: "-",
+    category: "wirid",
+    description: "Yasiin,Tahlil",
+    pdf: "pdf/تهليل كامل.pdf"
+  },
+  {
+  title: "sifat 20",
+    author: "Habib Utsman Bin Yahya",
+    category: "tauhid",
+    description: "tauhid",
+    pdf: "pdf/Kitab Sifat Dua Puluh.pdf"
   }
 
 ];
